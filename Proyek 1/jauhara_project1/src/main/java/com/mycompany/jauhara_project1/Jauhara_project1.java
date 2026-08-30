171d2fb16a6c8b16762cbbb7563f6be742314b85
Jauhara Putri <jauhara@contoh.com>
2026-08-30 19:17:00 +0700

    Tugas Modul 2

---
+++ Proyek 1/jauhara_project1/src/main/java/com/mycompany/jauhara_project1/Jauhara_project1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jauhara_project1;
+
+/**
+ *
+ * @author moonsrex_x
+ */
+public class Jauhara_project1 {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Jauhara Putri Al Hasna");
+        System.out.println("NPM     : 2505060032");
+        System.out.println("Alamat  : Perumnas Kalinegoro, Jl.Salak 3");
+        System.out.println("No_HP   : 088298127669");
+    }
+}
